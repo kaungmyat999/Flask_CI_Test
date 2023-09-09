36b30035795d2fd21d53de348fbc6b4b392e8c76
kaungmyat999 <kaungmyat30519991@gmail.com>
2023-09-09 16:49:25 -0400

    Added Requried Files For Building

--- src/app.py
+++ src/app.py
@@ -7,13 +7,24 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
+    <div style='text-align:center;margin-top:150px'>
+        <h2>Please Enter the Input and it will Produce What you Entered.</h2>
+   
+        <form action="/echo_user_input" method="POST">
+            <input name="user_input">
+            <input type="submit" value="Submit!">
+        </form>
+    </div>
+ 
      '''
+
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    return f'''
+    <div style='text-align:center;margin-top:150px'>
+        <h2>You entered: {input_text}</h2>
+    </div>
+
+    '''
